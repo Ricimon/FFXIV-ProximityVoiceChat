2ab0a879439cf8031b9f7eaa2ade7a82b3c92677
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-02-09 15:01:27 -0600

    fix table scopes

--- DalamudPlugin/ProximityVoiceChat/UI/View/ConfigWindow.cs
+++ DalamudPlugin/ProximityVoiceChat/UI/View/ConfigWindow.cs
@@ -139,33 +139,35 @@ public class ConfigWindow : Window, IPluginUIView, IDisposable
         using var deviceTab = ImRaii.TabItem("Audio Devices");
         if (!deviceTab) return;
 
-        using var deviceTable = ImRaii.Table("AudioDevices", 2);
-        if (deviceTable)
+        using (var deviceTable = ImRaii.Table("AudioDevices", 2))
         {
-            ImGui.TableSetupColumn("AudioDevicesCol1", ImGuiTableColumnFlags.WidthFixed, 80);
-            ImGui.TableSetupColumn("AudioDevicesCol2", ImGuiTableColumnFlags.WidthFixed, 230);
-
-            ImGui.TableNextRow(); ImGui.TableNextColumn();
-
-            ImGui.AlignTextToFramePadding();
-            ImGui.Text("Input Device"); ImGui.TableNextColumn();
-            ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
-            this.inputDevices ??= this.audioDeviceController.GetAudioRecordingDevices().ToArray();
-            var inputDeviceIndex = this.SelectedAudioInputDeviceIndex.Value + 1;
-            if (ImGui.Combo("##InputDevice", ref inputDeviceIndex, this.inputDevices, this.inputDevices.Length))
+            if (deviceTable)
             {
-                this.SelectedAudioInputDeviceIndex.Value = inputDeviceIndex - 1;
-            }
+                ImGui.TableSetupColumn("AudioDevicesCol1", ImGuiTableColumnFlags.WidthFixed, 80);
+                ImGui.TableSetupColumn("AudioDevicesCol2", ImGuiTableColumnFlags.WidthFixed, 230);
 
-            ImGui.TableNextRow(); ImGui.TableNextColumn();
-            ImGui.AlignTextToFramePadding();
-            ImGui.Text("Output Device"); ImGui.TableNextColumn();
-            ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
-            this.outputDevices ??= this.audioDeviceController.GetAudioPlaybackDevices().ToArray();
-            var outputDeviceIndex = this.SelectedAudioOutputDeviceIndex.Value + 1;
-            if (ImGui.Combo("##OutputDevice", ref outputDeviceIndex, this.outputDevices, this.outputDevices.Length))
-            {
-                this.SelectedAudioOutputDeviceIndex.Value = outputDeviceIndex - 1;
+                ImGui.TableNextRow(); ImGui.TableNextColumn();
+
+                ImGui.AlignTextToFramePadding();
+                ImGui.Text("Input Device"); ImGui.TableNextColumn();
+                ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
+                this.inputDevices ??= this.audioDeviceController.GetAudioRecordingDevices().ToArray();
+                var inputDeviceIndex = this.SelectedAudioInputDeviceIndex.Value + 1;
+                if (ImGui.Combo("##InputDevice", ref inputDeviceIndex, this.inputDevices, this.inputDevices.Length))
+                {
+                    this.SelectedAudioInputDeviceIndex.Value = inputDeviceIndex - 1;
+                }
+
+                ImGui.TableNextRow(); ImGui.TableNextColumn();
+                ImGui.AlignTextToFramePadding();
+                ImGui.Text("Output Device"); ImGui.TableNextColumn();
+                ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
+                this.outputDevices ??= this.audioDeviceController.GetAudioPlaybackDevices().ToArray();
+                var outputDeviceIndex = this.SelectedAudioOutputDeviceIndex.Value + 1;
+                if (ImGui.Combo("##OutputDevice", ref outputDeviceIndex, this.outputDevices, this.outputDevices.Length))
+                {
+                    this.SelectedAudioOutputDeviceIndex.Value = outputDeviceIndex - 1;
+                }
             }
         }
 
@@ -212,108 +214,110 @@ public class ConfigWindow : Window, IPluginUIView, IDisposable
         using var falloffTab = ImRaii.TabItem("Audio Falloff");
         if (!falloffTab) return;
 
-        using var falloffTable = ImRaii.Table("AudioFalloff", 2);
-        if (!falloffTable) return;
+        using (var falloffTable = ImRaii.Table("AudioFalloff", 2))
+        {
+            if (!falloffTable) return;
 
-        ImGui.TableSetupColumn("AudioFalloffSettingsCol1", ImGuiTableColumnFlags.WidthFixed, 140);
-        ImGui.TableSetupColumn("AudioFalloffSettingsCol2", ImGuiTableColumnFlags.WidthFixed, 150);
+            ImGui.TableSetupColumn("AudioFalloffSettingsCol1", ImGuiTableColumnFlags.WidthFixed, 140);
+            ImGui.TableSetupColumn("AudioFalloffSettingsCol2", ImGuiTableColumnFlags.WidthFixed, 150);
 
-        ImGui.TableNextRow(); ImGui.TableNextColumn();
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text("Master Volume"); ImGui.TableNextColumn();
-        ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
-        var masterVolume = this.MasterVolume.Value * 100.0f;
-        if (ImGui.SliderFloat("##MasterVolume", ref masterVolume, 0.0f, 500.0f, "%1.0f%%"))
-        {
-            this.MasterVolume.Value = masterVolume / 100.0f;
-        }
+            ImGui.TableNextRow(); ImGui.TableNextColumn();
+            ImGui.AlignTextToFramePadding();
+            ImGui.Text("Master Volume"); ImGui.TableNextColumn();
+            ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
+            var masterVolume = this.MasterVolume.Value * 100.0f;
+            if (ImGui.SliderFloat("##MasterVolume", ref masterVolume, 0.0f, 500.0f, "%1.0f%%"))
+            {
+                this.MasterVolume.Value = masterVolume / 100.0f;
+            }
 
-        ImGui.TableNextRow(); ImGui.TableNextColumn();
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text("Falloff Type"); ImGui.TableNextColumn();
-        ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
-        var falloffType = (int)this.AudioFalloffType.Value;
-        if (ImGui.Combo("##AudioFalloffType", ref falloffType, this.falloffTypes, this.falloffTypes.Length))
-        {
-            this.AudioFalloffType.Value = (AudioFalloffModel.FalloffType)falloffType;
-        }
+            ImGui.TableNextRow(); ImGui.TableNextColumn();
+            ImGui.AlignTextToFramePadding();
+            ImGui.Text("Falloff Type"); ImGui.TableNextColumn();
+            ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
+            var falloffType = (int)this.AudioFalloffType.Value;
+            if (ImGui.Combo("##AudioFalloffType", ref falloffType, this.falloffTypes, this.falloffTypes.Length))
+            {
+                this.AudioFalloffType.Value = (AudioFalloffModel.FalloffType)falloffType;
+            }
 
-        ImGui.TableNextRow(); ImGui.TableNextColumn();
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text("Minimum Distance");
-        if (ImGui.IsItemHovered())
-        {
-            ImGui.SetTooltip("Volume is max when below this distance, in yalms");
-        }
-        ImGui.TableNextColumn();
-        ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
-        var minDistance = this.AudioFalloffMinimumDistance.Value;
-        if (ImGui.InputFloat("##AudioFalloffMinimumDistance", ref minDistance, 0.1f, 1.0f, "%.1f"))
-        {
-            this.AudioFalloffMinimumDistance.Value = minDistance;
-        }
+            ImGui.TableNextRow(); ImGui.TableNextColumn();
+            ImGui.AlignTextToFramePadding();
+            ImGui.Text("Minimum Distance");
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetTooltip("Volume is max when below this distance, in yalms");
+            }
+            ImGui.TableNextColumn();
+            ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
+            var minDistance = this.AudioFalloffMinimumDistance.Value;
+            if (ImGui.InputFloat("##AudioFalloffMinimumDistance", ref minDistance, 0.1f, 1.0f, "%.1f"))
+            {
+                this.AudioFalloffMinimumDistance.Value = minDistance;
+            }
 
-        ImGui.TableNextRow(); ImGui.TableNextColumn();
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text("Maximum Distance");
-        if (ImGui.IsItemHovered())
-        {
-            ImGui.SetTooltip("Volume is 0 when above this distance, in yalms");
-        }
-        ImGui.TableNextColumn();
-        ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
-        var maxDistance = this.AudioFalloffMaximumDistance.Value;
-        if (ImGui.InputFloat("##AudioFalloffMaximumDistance", ref maxDistance, 0.1f, 1.0f, "%.1f"))
-        {
-            this.AudioFalloffMaximumDistance.Value = maxDistance;
-        }
+            ImGui.TableNextRow(); ImGui.TableNextColumn();
+            ImGui.AlignTextToFramePadding();
+            ImGui.Text("Maximum Distance");
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetTooltip("Volume is 0 when above this distance, in yalms");
+            }
+            ImGui.TableNextColumn();
+            ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
+            var maxDistance = this.AudioFalloffMaximumDistance.Value;
+            if (ImGui.InputFloat("##AudioFalloffMaximumDistance", ref maxDistance, 0.1f, 1.0f, "%.1f"))
+            {
+                this.AudioFalloffMaximumDistance.Value = maxDistance;
+            }
 
-        ImGui.TableNextRow(); ImGui.TableNextColumn();
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text("Falloff Factor");
-        if (ImGui.IsItemHovered())
-        {
-            ImGui.SetTooltip("The higher this number, the quicker volume drops off over distance. This value is not used in linear falloff.");
-        }
-        ImGui.TableNextColumn();
-        ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
-        var falloffFactor = this.AudioFalloffFactor.Value;
-        if (ImGui.InputFloat("##AudioFalloffFactor", ref falloffFactor, 0.1f, 1.0f, "%.1f"))
-        {
-            this.AudioFalloffFactor.Value = falloffFactor;
-        }
+            ImGui.TableNextRow(); ImGui.TableNextColumn();
+            ImGui.AlignTextToFramePadding();
+            ImGui.Text("Falloff Factor");
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetTooltip("The higher this number, the quicker volume drops off over distance. This value is not used in linear falloff.");
+            }
+            ImGui.TableNextColumn();
+            ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
+            var falloffFactor = this.AudioFalloffFactor.Value;
+            if (ImGui.InputFloat("##AudioFalloffFactor", ref falloffFactor, 0.1f, 1.0f, "%.1f"))
+            {
+                this.AudioFalloffFactor.Value = falloffFactor;
+            }
 
-        ImGui.TableNextRow(); ImGui.TableNextColumn();
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text("Mute Dead Players");
-        ImGui.TableNextColumn();
-        ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
-        var muteDeadPlayers = this.MuteDeadPlayers.Value;
-        if (ImGui.Checkbox("##MuteDeadPlayers", ref muteDeadPlayers))
-        {
-            this.MuteDeadPlayers.Value = muteDeadPlayers;
-        }
-        ImGui.SameLine();
-        var muteDeadPlayersDelayMs = this.MuteDeadPlayersDelayMs.Value;
-        ImGui.Text("Delay (ms)");
-        ImGui.SameLine(); ImGui.SetNextItemWidth(50);
-        if (ImGui.InputInt("##Delay (ms)", ref muteDeadPlayersDelayMs, 0))
-        {
-            this.MuteDeadPlayersDelayMs.Value = muteDeadPlayersDelayMs;
-        }
+            ImGui.TableNextRow(); ImGui.TableNextColumn();
+            ImGui.AlignTextToFramePadding();
+            ImGui.Text("Mute Dead Players");
+            ImGui.TableNextColumn();
+            ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
+            var muteDeadPlayers = this.MuteDeadPlayers.Value;
+            if (ImGui.Checkbox("##MuteDeadPlayers", ref muteDeadPlayers))
+            {
+                this.MuteDeadPlayers.Value = muteDeadPlayers;
+            }
+            ImGui.SameLine();
+            var muteDeadPlayersDelayMs = this.MuteDeadPlayersDelayMs.Value;
+            ImGui.Text("Delay (ms)");
+            ImGui.SameLine(); ImGui.SetNextItemWidth(50);
+            if (ImGui.InputInt("##Delay (ms)", ref muteDeadPlayersDelayMs, 0))
+            {
+                this.MuteDeadPlayersDelayMs.Value = muteDeadPlayersDelayMs;
+            }
 
-        ImGui.TableNextRow(); ImGui.TableNextColumn();
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text("Mute Out Of Map Players");
-        ImGui.TableNextColumn();
-        ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
-        ImGui.BeginDisabled(this.voiceRoomManager.InPublicRoom);
-        var muteOutOfMapPlayers = this.voiceRoomManager.InPublicRoom || this.MuteOutOfMapPlayers.Value;
-        if (ImGui.Checkbox("##MuteOutOfMapPlayers", ref muteOutOfMapPlayers))
-        {
-            this.MuteOutOfMapPlayers.Value = muteOutOfMapPlayers;
+            ImGui.TableNextRow(); ImGui.TableNextColumn();
+            ImGui.AlignTextToFramePadding();
+            ImGui.Text("Mute Out Of Map Players");
+            ImGui.TableNextColumn();
+            ImGui.SetNextItemWidth(ImGui.GetColumnWidth());
+            ImGui.BeginDisabled(this.voiceRoomManager.InPublicRoom);
+            var muteOutOfMapPlayers = this.voiceRoomManager.InPublicRoom || this.MuteOutOfMapPlayers.Value;
+            if (ImGui.Checkbox("##MuteOutOfMapPlayers", ref muteOutOfMapPlayers))
+            {
+                this.MuteOutOfMapPlayers.Value = muteOutOfMapPlayers;
+            }
+            ImGui.EndDisabled();
+            ImGui.SameLine(); Util.Common.HelpMarker("Can only disable in private rooms");
         }
-        ImGui.EndDisabled();
-        ImGui.SameLine(); Util.Common.HelpMarker("Can only disable in private rooms");
     }
 }
